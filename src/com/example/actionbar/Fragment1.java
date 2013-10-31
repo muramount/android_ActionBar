405c6a4d317b5d791b8314fa0b70a9a1aff1bebe
tmurayama <tmurayama@BEPC-6063.ad01.bemap.jp>
2013-10-31 12:08:54 +0900

    add log.

--- src/com/example/actionbar/Fragment1.java
+++ src/com/example/actionbar/Fragment1.java
@@ -1,5 +1,6 @@
 package com.example.actionbar;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,21 +13,69 @@ public class Fragment1 extends Fragment {
     private static final String TAG = Fragment1.class.getName();
 
     @Override
+    public void onAttach(Activity act){
+        super.onAttach(act);
+        Log.d(TAG,"Fragment-onAttach");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG,"Fragment-onCreate");
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView");
         return inflater.inflate(R.layout.fragment1, container, false);
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart");
+    public void onActivityCreated(Bundle bundle){
+        super.onActivityCreated(bundle);
+        Log.d(TAG,"Fragment-onActivityCreated");
     }
 
     @Override
-    public void onStop() {
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG,"Fragment-onStart");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG,"Fragment-onResume");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG,"Fragment-onPause");
+    }
+
+    @Override
+    public void onStop(){
         super.onStop();
-        Log.d(TAG, "onStop");
+        Log.d(TAG,"Fragment-onStop");
+    }
+
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+        Log.d(TAG,"Fragment-onDestroyView");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"Fragment-onDestroy");
+    }
+
+    @Override
+    public void onDetach(){
+        super.onDetach();
+        Log.d(TAG,"Fragment-onDetach");
     }
 
 }
